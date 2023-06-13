d13cce4a6461e310f6f94c71de4f2dfdb4defc65
Oleksii Honchar <tuiteraz@gmail.com>
2023-06-13 21:01:04 +0200

    feat(tailwind): added tw navbar with custom gradients

---
+++ src/pages/Root/components/callsToAction.ts
@@ -0,0 +1,6 @@
+import { PhoneIcon, PlayCircleIcon } from "@heroicons/react/20/solid/index.js";
+
+export const callsToAction = [
+  { name: "Watch demo", href: "#", icon: PlayCircleIcon },
+  { name: "Contact sales", href: "#", icon: PhoneIcon },
+];
